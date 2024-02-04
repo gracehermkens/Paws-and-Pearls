8bf646698a8c02dd51da23e3af7f13d6e801f5ff
Vish <vvm33@drexel.edu>
2024-02-03 21:13:55 -0500

    feat: boilerplate and greeting screen

---
+++ core/src/info/vivime/bobacafe/Game.java
@@ -0,0 +1,31 @@
+package info.vivime.bobacafe;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Game extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("technical_difficulties.png");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0.1f, 0.1f, 0.1f, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
