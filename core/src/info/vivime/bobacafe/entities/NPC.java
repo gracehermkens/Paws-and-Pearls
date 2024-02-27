1da25d4082e4db3e0382e1c1f8aa71bc96233322
selha <sae65@drexel.edu>
2024-02-27 12:10:40 -0500

    spawn of NPC

---
+++ core/src/info/vivime/bobacafe/entities/NPC.java
@@ -0,0 +1,22 @@
+package info.vivime.bobacafe.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class NPC extends Actor {
+    private Texture texture;
+
+    public NPC(Vector2 startPosition) {
+        super();
+        texture = (new Texture("catCustomer.PNG"));
+        setPosition(startPosition.x, startPosition.y);
+    }
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(this.texture, getX(), getY());
+    }
+
+}
--- core/src/info/vivime/bobacafe/screen/TestScreen.java
+++ core/src/info/vivime/bobacafe/screen/TestScreen.java
@@ -4,18 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import info.vivime.bobacafe.BobaCafe;
+import info.vivime.bobacafe.entities.NPC;
 
 public class TestScreen extends BaseScreen {
     SpriteBatch batch;
     Texture image;
+    Stage stage;
 
     public TestScreen(final BobaCafe game) {
         super(game);
         batch = new SpriteBatch();
+        stage = new Stage();
 
         image = new Texture("cafeBackground.PNG");
+        NPC npc = new NPC(new Vector2(740, 80));
+        stage.addActor(npc);
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -24,11 +32,15 @@ public class TestScreen extends BaseScreen {
         batch.begin();
         batch.draw(image, 0, 0);
         batch.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         image.dispose();
+        stage.dispose();
     }
 }
