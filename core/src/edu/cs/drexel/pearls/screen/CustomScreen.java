5532f066be451a78f28662659430b77bdb709087
gracehermkens <graher87@drexel.edu>
2024-05-29 15:12:44 -0400

    added custom screen (comes after title screen)

--- core/src/edu/cs/drexel/pearls/screen/CustomScreen.java
+++ core/src/edu/cs/drexel/pearls/screen/CustomScreen.java
@@ -1,47 +1,33 @@
 package edu.cs.drexel.pearls.screen;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import edu.cs.drexel.pearls.BobaCafe;
-
-public class TitleScreen extends BaseScreen {
-
+public class CustomScreen extends BaseScreen {
     SpriteBatch batch;
-    Texture titleImage;
-    Texture titleBackground;
+    Texture customScreen;
     float time = 0;
 
-    public TitleScreen(final BobaCafe game) {
+    public CustomScreen(final BobaCafe game) {
         super(game);
         batch = new SpriteBatch();
-
-        titleImage = new Texture("titleScreen.png");
-        titleBackground = new Texture("titleScreenFinal.png");
+        customScreen = new Texture("customScreen.png");
     }
-
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0.9f, 0.9f, 0.9f, 1);
         batch.begin();
-        batch.draw(titleBackground, 0, 0);
-        batch.draw(titleImage, 0, 0);
+        batch.draw(customScreen, 0, 0);
         batch.end();
 
         time += delta;
         if (time > 1) {
             if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY) || Gdx.input.justTouched()) {
-                game.setScreen(new CustomScreen(game));
+                game.setScreen(new CafeScreen(game));
             }
         }
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        titleImage.dispose();
     }
 }
